7df4311764e1e3aa3ebcfe0d2519d892161df9fb
Emily <bruner.emilym@gmail.com>
2020-01-07 13:26:50 -0500

    created dashboard draft

--- chef-portfolio/src/components/HomeDashboard.js
+++ chef-portfolio/src/components/HomeDashboard.js
@@ -1,2 +1,36 @@
-import React from 'react'
-import axios from 'axios'
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+
+import {fetchRecipes} from '../store/actions';
+
+import RecipeCard from './RecipeCard';
+
+const HomeDashboard = ({fetchRecipes, recipes, error, isFetching}) => {
+    useEffect(() => {
+        fetchRecipes();
+    }, [fetchRecipes])
+
+    console.log(recipes);
+
+    if (isFetching) {
+        return <h2>Loading Recipes...</h2>
+    }
+    return (
+        <div>
+            {error && <p>{error}</p>}
+            {recipes.map(recipe => (
+                <RecipeCard recipe={recipe}/>
+            ))}
+        </div>
+    );
+}
+
+const mapStateToProps = state => {
+    return {
+        recipes: state.recipes,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+};
+
+export default connect (mapStateToProps, {fetchRecipes})(HomeDashboard);
