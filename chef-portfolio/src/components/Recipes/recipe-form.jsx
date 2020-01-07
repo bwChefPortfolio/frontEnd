bc7ef55a637e2cc8ef78f8a0d4f1a7c91dab3cf9
Logan Sorensen <loganmsorensen@gmail.com>
2020-01-07 12:42:15 -0800

    improved form validation for recipe form

--- chef-portfolio/src/components/Recipes/recipe-form.jsx
+++ chef-portfolio/src/components/Recipes/recipe-form.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 import './recipe-form.scss';
 
 const RecipeForm = ({ values, errors, touched, status }) => {
-    const handleSubmit = event => {
-        event.preventDefault();
-    }
+    // console.log("values", values);
+    // console.log("errors", errors);
+    // console.log("touched", touched);
+    const [recipe, setRecipe] = useState([]);
+    useEffect(() => {
+        console.log("status has changed", status);
+        status && setRecipe(recipe => [...recipe, status]);
+    }, [status]);
 
     return (
         <div className='recipe-container'>
             <img />
-            <Form className='recipe-form' onSubmit={handleSubmit}>
+            <Form className='recipe-form'>
                 <div className='labels'>
                     <label htmlFor='title'>Recipe Name: </label>
                     <label htmlFor='type'>Meal Type: </label>
@@ -27,13 +33,14 @@ const RecipeForm = ({ values, errors, touched, status }) => {
                         />{touched.title && errors.title && (<p className='error'>{errors.title}</p>)}
                     </div>
                     <div className='field'>
-                        <select className='select' id='type'>
-                        <option>Breakfast</option>
-                        <option>Lunch</option>
-                        <option>Dinner</option>
-                        <option>Snacks</option>
-                        <option>Dessert</option>
-                        </select>
+                        <Field as='select' className='select' id='type' name='type'>
+                        <option value='none' selected disabled hidden>Choose an option</option>
+                        <option value='Breakfast'>Breakfast</option>
+                        <option value='Lunch'>Lunch</option>
+                        <option value='Dinner'>Dinner</option>
+                        <option value='Snacks'>Snacks</option>
+                        <option value='Dessert'>Dessert</option>
+                        </Field>{touched.type && errors.type && (<p className='error'>{errors.type}</p>)}
                     </div>
                     <div className='field'>
                         
@@ -54,16 +61,48 @@ const RecipeForm = ({ values, errors, touched, status }) => {
                     <button type='submit'>Add Recipe</button>
                 </div>
             </Form>
+            {recipe.map(dish => {
+                return(
+                    <ul key={dish.id}>
+                        <li>Title: {dish.title}</li>
+                        <li>Meal Type: {dish.type}</li>
+                        <li>Ingredients: {dish.ingredients}</li>
+                        <li>Recipe: {dish.recipe}</li>
+                    </ul>
+                )
+            })}
         </div>
     )
 }
 
 const FormikRecipeForm = withFormik({
+    mapPropsToValues(props) {
+        return {
+            title: props.title || "",
+            type: props.type,
+            ingredients: props.ingredients || "",
+            recipe: props.recipe || ""
+        }
+    },
     validationSchema: Yup.object().shape({
-        title: Yup.string().required("Please add a name to your dish"),
-        ingredients: Yup.string().required("Please add at least one ingredient"),
-        recipe: Yup.string().required("Please provide a recipe for your dish")
-    })
+        title: Yup.string().required("*Please add a name to your dish"),
+        type: Yup.string().oneOf(["Breakfast", "Lunch", "Dinner", "Snacks", "Dessert"]).required("*Please select a meal type"),
+        ingredients: Yup.string().required("*Please add at least one ingredient"),
+        recipe: Yup.string().required("*Please provide a recipe for your dish")
+    }),
+    handleSubmit(values, {setStatus, resetForm}) {
+        console.log("submitting", values);
+        axios
+            .post("https://reqres.in/api/users/", values)
+            .then(response => {
+                console.log("success", response);
+                setStatus(response.data);
+                resetForm();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+    }
 })(RecipeForm)
 
 export default FormikRecipeForm;
