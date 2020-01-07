bc05bfc9f45fb144c9707468cf5bbd0403e44f95
Emily <bruner.emilym@gmail.com>
2020-01-07 13:16:00 -0500

    created reducers in redux store

--- chef-portfolio/src/store/actions.js
+++ chef-portfolio/src/store/actions.js
@@ -7,10 +7,12 @@ export const START_FETCHING = "START_FETCHING";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAULURE";
 
-export const fetchRecupes = () => {
+export const fetchRecipes = () => {
     //action objects
     dispatchEvent({ type: START_FETCHING});
-    axios.get(//insert recipe link here)
+    axios.get(
+        //need backend link for recipes
+        )
     .then(res => dispatchEvent({ type: FETCH_SUCCESS, payload: res.data}))
     .then(res => console.log(res.data))
     .catch(err => dispatchEvent({type: FETCH_FAILURE, payload: err.response}));
