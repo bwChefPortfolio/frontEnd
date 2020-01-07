170345d09fad0c3790fa464534e92dba554fcf97
Emily <bruner.emilym@gmail.com>
2020-01-07 13:06:44 -0500

    created fetching actions in redux store

---
+++ chef-portfolio/src/store/actions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+
+//action types
+
+export const START_FETCHING = "START_FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAULURE";
+
+export const fetchRecupes = () => {
+    //action objects
+    dispatchEvent({ type: START_FETCHING});
+    axios.get(//insert recipe link here)
+    .then(res => dispatchEvent({ type: FETCH_SUCCESS, payload: res.data}))
+    .then(res => console.log(res.data))
+    .catch(err => dispatchEvent({type: FETCH_FAILURE, payload: err.response}));
+};
